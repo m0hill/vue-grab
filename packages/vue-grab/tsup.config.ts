cf391031627b5a8846cec877dce4adf865497685
Mohil <112541468+m0hill@users.noreply.github.com>
2025-10-25 05:55:16 +0900

    ref: minor

--- packages/vue-grab/tsup.config.ts
+++ packages/vue-grab/tsup.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, type Options } from "tsup";
 const banner = `/**
  * @license MIT
  *
- * Copyright (c) 2025 Aiden Bai
+ * Copyright (c) 2025 Mohil Garg
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
